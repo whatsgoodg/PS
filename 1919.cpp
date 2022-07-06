5690557417a6e99eee3a52bd00b7c1ea91809f58
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2022-07-06 15:50:31 +0900

    0x03-6(배열)

---
+++ 1919.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int alpha[26]{};
+	int alpha2[26]{};
+
+	string a, b; cin >> a >> b;
+
+	for (int i = 0; i < a.size(); i++) alpha[a[i] - 97]++;
+	for (int i = 0; i < b.size(); i++) alpha2[b[i] - 97]++;
+
+	int cnt = 0;
+	for (int i = 0; i < b.size(); i++) {
+		int differ = alpha[b[i] - 97] - alpha2[b[i] - 97];
+		if (differ != 0) {
+			cnt += abs(differ);
+			alpha[b[i] - 97] = alpha2[b[i] - 97] = 0;
+		}
+	}
+	for (int i = 0; i < a.size(); i++) {
+		int differ = alpha[a[i] - 97] - alpha2[a[i] - 97];
+		if (differ != 0) {
+			cnt += abs(differ);
+			alpha[a[i] - 97] = alpha2[a[i] - 97] = 0;
+		}
+	}
+	cout << cnt << "\n";
+
+	
+	
+}
