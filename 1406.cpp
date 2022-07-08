2f7929945928bd52fb5adf16a5800120950b5c15
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2022-07-08 21:28:35 +0900

    0x04-1(리스트 STL x)
    리스트 구현을 배열 세 개(원소, 전, 후)로 한다. 
    예외는 맨 마지막 자리에 원소가 삽입되거나 맨 마지막 원소가 삭제될 때 이다.(#1,2)

--- 1406.cpp
+++ 1406.cpp
@@ -49,11 +49,11 @@ int main() {
 	for (int i = 0; i < n; i++) {
 		cin >> in2;
 		if (in2 == 'L') {
-			if (pre[cursor] != -1) //맨 왼쪽일 경우
+			if (pre[cursor] != -1) //맨 왼쪽일 경우 커서 움직임x
 				cursor = pre[cursor];
 		}
 		else if (in2 == 'D') {
-			if (nxt[cursor] != -1)//맨 오른쪽일 경우
+			if (nxt[cursor] != -1)//맨 오른쪽일 경우 커서 움직임x
 				cursor = nxt[cursor];
 		}	
 		else if (in2 == 'B') {//맨 왼쪽일 경우
