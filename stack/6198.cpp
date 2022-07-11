819ff87e31e0fc130ccfff6c656894a181761a7e
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2022-07-11 23:21:00 +0900

    0x05(stack)-5
    테스트 케이스의 범위를 따져 long을 만들어줬어야 한다.

---
+++ stack/6198.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stack>
+using namespace std;
+
+stack<int> S;
+int N;
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	cin >> N;
+	int v;
+	long ans = 0;
+	S.push(0);
+	for (int i = 0; i < N; i++) {
+		cin >> v;
+		while (!S.empty() && S.top() <= v) {
+			S.pop();
+		}
+		ans += S.size();
+		S.push(v);
+	}
+	cout << ans << "\n";
+}
