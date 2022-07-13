cfd46a97825750f4b3c95e73611ca0cd6f8beba5
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2022-07-13 23:52:33 +0900

    0x06(queue)-2
    back은 앞으로 움직이며 새로운 값을 추가.
    front는 맨 앞의 원소 전에 위치해 있음. 
    pop: ++front
    push: ++back
    둘 다 mod capacity를 이용하여 원형 큐 구현

---
+++ queue/18258.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+using namespace std;
+
+int N;
+int arr[2000001];
+int fst = 0;
+int lst = 0;
+int Size = 0;
+
+void push(int v) {
+	arr[++lst % 2000001] = v;
+	Size++;
+}
+int pop() {
+	Size--;
+	return arr[++fst % 2000001];
+}
+
+bool empty() { return Size == 0; }
+
+int front() { return arr[(fst + 1) % 2000001]; }
+
+int back() { return arr[lst]; }
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	cin >> N;
+
+	string in;
+	int v;
+	for (int i = 0; i < N; i++) {
+		cin >> in;
+		if (in == "push") {
+			cin >> v;
+			push(v);
+		}
+		else if (in == "pop") {
+			if (empty()) cout << -1 << "\n";
+			else cout << pop() << "\n";
+		}
+		else if (in == "front") {
+			if (empty()) cout << -1 << "\n";
+			else cout << front() << "\n";
+		}
+		else if (in == "back") {
+			if (empty()) cout << -1 << "\n";
+			else cout << back() << "\n";
+		}
+		else if (in == "size") {
+			cout << Size << "\n";
+		}
+		else
+			cout << empty() << "\n";
+	}
+}
