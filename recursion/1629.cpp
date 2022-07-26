ee4bcfb4e6f600e977efedcae43e6b4aff29c10d
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2022-07-26 23:22:20 +0900

    Create 1629.cpp

---
+++ recursion/1629.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+#define LL long long
+LL A, B, C;
+
+LL POW(int a, int b, int m) {
+	if (b == 1) return a % m;
+	LL val = POW(a, b / 2, m);
+	val = val * val % m;
+	if (b % 2 == 0) return val;
+	return val * a % m;
+}
+
+int main() {
+	ios::sync_with_stdio(0); cin.tie(0);
+	cin >> A >> B >> C;
+
+	cout << POW(A, B, C);
+
+}
