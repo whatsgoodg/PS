c34bfa5db9a194522030936c61d96dd40ed7482a
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2022-12-23 17:36:33 +0900

    0x03-1(배열)

---
+++ array/10807.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+
+	int n; cin >> n;
+
+	int* arr = new int[n];
+	for (int i = 0; i < n; i++) cin >> arr[i];
+
+	int val, count = 0; cin >> val;
+	for (int i = 0; i < n; i++) if (arr[i] == val) count++;
+
+	cout << count;
+}
