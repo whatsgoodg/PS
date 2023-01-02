bf167ff8bfb362420b0b89847762d891173c1424
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2023-01-02 17:17:10 +0900

    0x15-2

---
+++ hashtable/1620.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+unordered_map<string, int> s2i;
+string i2s[100005];
+
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
+  int n, m;
+  cin >> n >> m;
+  for(int i = 1; i <= n; i++){
+    cin >> i2s[i];
+    s2i[i2s[i]] = i;
+  }
+  while(m--){
+    string query;
+    cin >> query;
+    if(isdigit(query[0]))
+      cout << i2s[stoi(query)] << '\n';
+    else
+      cout << s2i[query] << '\n';
+  }
+}
