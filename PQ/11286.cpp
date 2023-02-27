d264cea62b72780ed1d165c117b878e42c75747e
Ki-Jeong Kim <86244920+whatsgoodg@users.noreply.github.com>
2023-02-27 13:52:15 +0900

    Update 11286.cpp

--- PQ/11286.cpp
+++ PQ/11286.cpp
@@ -1,48 +1,18 @@
-#include <iostream>
-#include <queue>
-using namespace std;
-
 class cmp {
 public:
 	bool operator() (int a, int b) { //a 음수 b 음수
 		if (a < 0 && b < 0) {
 			return a > b;
 		}
+		else if (a < 0 && b > 0) {  //a 음수 b 양수
+			return -1 * a > b;
+		}	
 		else if (a > 0 && b < 0) {  //a 양수 b 음수
 			b = -1 * b;
-			return a < b;
+			return a >= b;
 		}
-		else if (a < 0 && b > 0) {  //a 음수 b 양수 같을 때 true를 반환하지만 런에러x 
-			return -1 * a <= b;
-		} 
 		else {  //a 양수 b 양수
-			return a < b;
+			return a > b;
 		}
 	}
 };
-
-priority_queue<int, vector<int>, cmp> PQ;
-int n;
-int val;
-
-int main() {
-	ios::sync_with_stdio(0);
-	cin.tie(0);
-
-	cin >> n;
-	while (n--) {
-		cin >> val;
-		if (val != 0) {
-			PQ.push(val);
-		}
-		else {
-			if (PQ.empty()) cout << 0;
-			else {
-				cout << PQ.top();
-				PQ.pop();
-			}
-			cout << '\n';
-	
-		}
-	}
-}
